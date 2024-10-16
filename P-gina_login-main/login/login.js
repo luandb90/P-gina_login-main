ac834ee0f4bc43150e3ff3325303dbe5b250b329
Luandb90 <luandb90@gmail.com>
2024-10-15 21:33:56 -0300

    V2.1

--- P-gina_login-main/login/login.js
+++ P-gina_login-main/login/login.js
@@ -1,44 +1,13 @@
-let password = document.getElementById ('password');
-let icon = document.getElementById ('icon');
+function validateFields(){
+    document.getElementById("email").value;
+    if (!email){
+        document.getElementById("esqueci_senha").disabled+ true;
+    } else if (validateEmail (email)) {
+        document.getElementById("esqueci_senha").disabled+ false;
 
-function showHide(){
-    if(password.type === 'password'){
-        password.setAttribute('type', 'text');
-        icon.classList.add('hide')
     }
-    else{
-        password.setAttribute('type', 'password');
-        icon.classList.remove ('hide')
+
+    function validateEmail(email){
+        return /\S+@\S+\.\S+/.test(email);
+    } 
     }
-}
-
-
-function logar(){
-    var email = document.getElementById('email').value
-    var password = document.getElementById('password').value
-
-    console.log(JSON.stringify({
-        email: email,
-        password: password
-    }));
-
-    fetch("login",{
-        method: 'POST',
-        body: JSON.stringify({
-            email: email,
-            password: password
-        }),
-        headers: {"content-type" : "application/json"}
-    })
-
-    .then(async(resp) =>{
-        var status = await resp.text();
-        console.log(status)
-        if(status == 'conectado'){
-            location.href = '/acesso.html'
-        }else{
-            alert('nome ou senha invalidos!')
-        }
-    })
-
-}
